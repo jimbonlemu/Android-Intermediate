f0808f67d4990016db2405192c21996dcd6ac018
perompak_kode <iqbaltech29@gmail.com>
2024-04-27 15:16:01 +0700

    implement seatsView

--- app/src/main/java/com/jimbonlemu/mycustomview/MainActivity.kt
+++ app/src/main/java/com/jimbonlemu/mycustomview/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jimbonlemu.mycustomview
 
 import android.os.Bundle
+import android.view.Window
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jimbonlemu.mycustomview.databinding.ActivityMainBinding
 
@@ -8,8 +10,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
+            supportActionBar?.hide()
+            finishButton.setOnClickListener {
+                seatsView.seat?.let {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Kursi Anda nomor ${it.name}.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } ?: run {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Silahkan pilih kursi terlebih dahulu.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 }
